016179790b134b9125da0c5ebee7305fc7b5e150
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-01-20 17:09:55 +0100

    Enhance loading state in BrandsCarousel with a styled spinner and gradient background

--- src/pages/Homepage/components/BrandsCarousel.jsx
+++ src/pages/Homepage/components/BrandsCarousel.jsx
@@ -33,7 +33,23 @@ export default function BrandsCarousel() {
         }
     }, [dispatch, status]);
 
-    if (status === 'loading') return <p>Loading...</p>;
+    if (status === 'loading') {
+        return (
+            <div className="relative bg-gradient-to-b from-neutral-900 to-neutral-800 overflow-hidden h-52 flex justify-center items-center">
+            <div className="absolute top-0 left-0 h-full w-full bg-gradient-to-r from-black/65 via-transparent to-black/65 z-10"></div>
+            <div
+                className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-neutral-100 motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                role="status"
+            >
+                <span
+                    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                >
+                    Loading...
+                </span>
+            </div>
+        </div>
+        );
+    }
     if (status === 'failed') return <p>Error: {error}</p>;
 
    return(
