8e12e24660718d59b9c5b42b96be87d2b8c69fe8
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-02-01 20:40:19 +0100

    Nothing

--- src/pages/Brands/components/SidePage.jsx
+++ src/pages/Brands/components/SidePage.jsx
@@ -7,17 +7,21 @@ export default function SidePage({children, path}) {
     }
 
     return(
-        <div 
-            to={path}
-            className='absolute left-0 top-16 z-40 px-20 py-8 bg-black/15 border w-[25rem] hover:w-[27rem] border-white border-l-0 transition-all ease-in-out duration-300 cursor-pointer hover:bg-white/10'
-            onClick={handleClick}
-            style={{
-                clipPath: click ? 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' : 'polygon(0 0, 100% 0, 87% 100%, 0 100%)',
-            }}
-        >
-            <span className="uppercase text-white font-bold text-5xl font-montserrat">
-                {children}
-            </span>
-        </div>
+        <>
+            <div 
+                to={path}
+                className='absolute left-0 top-16 z-40 px-20 py-8 bg-black/15 border w-[25rem] hover:w-[27rem] border-white border-l-0 transition-all ease-in-out duration-300 cursor-pointer hover:bg-white/10'
+                onClick={handleClick}
+                style={{
+                    clipPath: click ? 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' : 'polygon(0 0, 100% 0, 87% 100%, 0 100%)',
+                }}
+            >
+                <div 
+                    className="uppercase text-white font-bold w-full text-5xl font-montserrat"
+                >
+                    {children}
+                </div>
+            </div>
+        </>
     );
 }
