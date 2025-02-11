eaa0eaba22a63e37cdfdc9c3a60f67bc01bc174b
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-02-11 19:44:04 +0100

    ClipPath Bug Fixed

--- src/components/FilterComp.jsx
+++ src/components/FilterComp.jsx
@@ -1,11 +1,11 @@
 import Fuel from "./Filters/Fuel";
+import './FilterComp.css';
 
 export default function FilterComp() {
     return(
         <>
             <div 
-                className="bg-neutral-300 border border-white sticky z-20 shadow-md left-0 top-0 h-auto w-full grid grid-cols-12 px-[4%] font-kanit py-3 overflow-visible"
-                style={{ clipPath: 'polygon(0 0, 100% 0, 97% 100%, 3% 100%)', }}
+                className="filter-container py-3 px-[4%] grid grid-cols-12 sticky top-0 left-0 z-20"
             >
                 <Fuel />
                 <div>
--- src/components/Filters/Fuel.jsx
+++ src/components/Filters/Fuel.jsx
@@ -11,7 +11,7 @@ export default function Fuel() {
     }
 
     return (
-        <div className="w-full relative overflow-visible">
+        <div className="w-full relative">
             <div
                 onClick={handleDropDown}
                 className="uppercase font-bold flex items-center gap-3 w-2/3 cursor-pointer"
@@ -22,7 +22,10 @@ export default function Fuel() {
                     icon={faChevronRight}
                 />
             </div>
-            <ul className={`w-2/3 absolute left-0 bg-neutral-800 border border-white transition-all ease-in-out duration-300 overflow-visible z-50 ${dropDown ? 'max-h-48 opacity-100' : 'max-h-0 opacity-0'}`}>
+            <ul 
+                className={`w-2/3 absolute left-0 top-7 bg-neutral-800 border border-white transition-all ease-in-out duration-300 z-50 ${dropDown ? 'max-h-48 opacity-100' : 'max-h-0 opacity-0'}`}
+            
+            >
                 <ListDropDown>Benzina</ListDropDown>
                 <ListDropDown>Diesel</ListDropDown>
                 <ListDropDown>Elettrico</ListDropDown>
